f713214eebefe2c0ec21e3603225cb32bcb2f904
Jothi-SCS <54353240+Jothi-SCS@users.noreply.github.com>
2026-07-30 15:28:07 +0530

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,9 +1,12 @@
+-- Create Database
 CREATE DATABASE CollegeDB;
 
+-- Select the Database
 USE CollegeDB;
 
-CREATE TABLE Department(
-DepartmentID INT(5) PRIMARY KEY,
-DepartmentName VARCHAR(20),
-HOD VARCHAR(20)
+-- Create Department Table
+CREATE TABLE Department (
+    DepartmentID INT(5) PRIMARY KEY,
+    DepartmentName VARCHAR(20),
+    HOD VARCHAR(20)
 );
